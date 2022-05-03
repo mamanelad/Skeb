1191d8e81f0a2cc47122ff25f91eebfd49450102
tamirsagi891 <93332015+tamirsagi891@users.noreply.github.com>
2022-05-03 19:13:22 +0300

    Pushing new tilemap

--- Assets/Scripts/ColorWheelManager.cs
+++ Assets/Scripts/ColorWheelManager.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using JetBrains.Annotations;
 using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
 using UnityEngine.UI;
 
 public class ColorWheelManager : MonoBehaviour
