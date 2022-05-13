456be55194a85609b4f59e37f74c4d652bebb257
tamirsagi891 <93332015+tamirsagi891@users.noreply.github.com>
2022-05-13 04:05:23 +0300

    Added combat mechaincs

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,9 +3,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CharacterController : MonoBehaviour
+public class PlayerController : MonoBehaviour
 {
 
+    public static PlayerController _PlayerController;
     public enum PlayerState { Idle, Move, Combat }
     
     [SerializeField] private float movementSpeed = 5;
@@ -14,24 +15,37 @@ public class CharacterController : MonoBehaviour
     [SerializeField] private LayerMask dashLayerMask;
     
     private Rigidbody2D _rb;
-    private Animator _animator;
+    [NonSerialized] public Animator Animator;
     private PlayerState _playerState = PlayerState.Idle;
     private Vector2 _moveDirection = Vector2.zero;
     private Vector2 _idleDirection = Vector2.down;
     private bool _dashStatus;
+    [NonSerialized] public bool IsAttacking;
     private static readonly int State = Animator.StringToHash("State");
     private static readonly int WalkHorizontal = Animator.StringToHash("WalkHorizontal");
+    private static readonly int WalkVertical = Animator.StringToHash("WalkVertical");
+    private static readonly int IdleHorizontal = Animator.StringToHash("IdleHorizontal");
+    private static readonly int IdleVertical = Animator.StringToHash("IdleVertical");
+    private static readonly int AttackHorizontal = Animator.StringToHash("AttackHorizontal");
+    private static readonly int AttackVertical = Animator.StringToHash("AttackVertical");
+
+    private void Awake()
+    {
+        if (_PlayerController == null)
+            _PlayerController = this;
+    }
 
     void Start()
     {
         _rb = GetComponent<Rigidbody2D>();
-        _animator = GetComponent<Animator>();
+        Animator = GetComponent<Animator>();
     }
     
     private void Update()
     {
         SetPlayerState();
         SetMovementAndIdleDirection();
+        Attack();
         PlayAnimation();
         
         if (Input.GetKeyDown(KeyCode.LeftShift) && canDash)
@@ -41,10 +55,22 @@ public class CharacterController : MonoBehaviour
 
     private void SetPlayerState()
     {
+        if (_playerState == PlayerState.Combat)
+            return;
         if (_moveDirection.sqrMagnitude > 0.01f)
             _playerState = PlayerState.Move;
         else
             _playerState = PlayerState.Idle;
+    }
+
+    private void Attack()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && !IsAttacking)
+        {
+            IsAttacking = true;
+            _playerState = PlayerState.Combat;
+        }
+        
     }
 
     private void SetMovementAndIdleDirection()
@@ -58,11 +84,13 @@ public class CharacterController : MonoBehaviour
 
     private void PlayAnimation()
     {
-        _animator.SetInteger(State, (int) _playerState);
-        _animator.SetFloat("WalkHorizontal", _moveDirection.x);
-        _animator.SetFloat("WalkVertical", _moveDirection.y);
-        _animator.SetFloat("IdleHorizontal", _idleDirection.x);
-        _animator.SetFloat("IdleVertical", _idleDirection.y);
+        Animator.SetInteger(State, (int) _playerState);
+        Animator.SetFloat(WalkHorizontal, _moveDirection.x);
+        Animator.SetFloat(WalkVertical, _moveDirection.y);
+        Animator.SetFloat(IdleHorizontal, _idleDirection.x);
+        Animator.SetFloat(IdleVertical, _idleDirection.y);
+        Animator.SetFloat(AttackHorizontal, _idleDirection.x);
+        Animator.SetFloat(AttackVertical, _idleDirection.y);
     }
 
     private void FixedUpdate()
@@ -81,5 +109,10 @@ public class CharacterController : MonoBehaviour
             _rb.MovePosition(dashPosition);
             _dashStatus = false;
         }
+    }
+
+    public void SetPlayerState(PlayerState status)
+    {
+        _playerState = status;
     }
 }
